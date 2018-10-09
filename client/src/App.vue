49068296f04cbe409735434b93cde9537e128a5d
wdjcodes <wdjcodes@users.noreply.github.com>
2018-10-09 18:58:51 -0400

    Moved the nav bar to be a component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <a class="navbar-brand" href="#">Auth File Server</a>
-    </nav>
+    </nav> -->
+    <Navigation></Navigation>
     <router-view class="container pt-2"/>
   </div>
 </template>
+
+<script>
+  import Navigation from './components/Navigation.vue';
+  export default {
+    components: {
+    'Navigation': Navigation,
+    },
+  }
+</script>
+
 
 <style>
 
---
+++ client/src/components/Navigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div class="navbar-brand">nodeFiles</div>
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <router-link to="/login" class="nav-link">Login</router-link>
+        </li>
+      </ul>
+    <!-- <a class="navbar-brand" href="#">Auth File Server</a> -->
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
