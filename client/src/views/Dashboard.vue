ee3803b4bf3d32958aaac0fc362a8252d9efdeba
wdjcodes <wdjcodes@users.noreply.github.com>
2018-10-17 15:35:12 -0400

    Created linked cards for the dashboard

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,19 +1,49 @@
 <template>
   <section>
     <h1>Dashboard</h1>
-    <router-link
-      :to="{name: 'notes'}"
-      class="btn btn-primary">
-      Notes
-    </router-link>
+    <section class="row mt-3">
+      <div
+        class="col-4 mb-3 pr-0 pl-3"
+        v-for="page in pages"
+        :key="page.route">
+          <card-link :route="page.route">
+            <div class="card-header">
+              <div class="row">
+                <h4 class="col-10">{{page.name}}</h4>
+              </div>
+            </div>
+            <img
+              class="card-img-bottom p-2"
+              :src="page.imgUrl"
+              :alt="page.imgAltText">
+          </card-link>
+      </div>
+    </section>
   </section>
 </template>
 
 <script>
+import CardLink from '@/components/CardLink';
+
 export default {
+  data() {
+    return {
+      hover: false,
+      pages: [
+        {
+          name: 'Notes',
+          route: '/notes',
+          imgUrl: require('@/assets/notes.png'),
+          imgAltText: 'notes.png',
+        },
+      ]
+    };
+  },
+  components: {
+    CardLink,
+  },
 }
 </script>
 
-<style>
-
+<style lang=css>
 </style>
