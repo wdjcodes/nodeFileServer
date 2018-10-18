e5900ad914fb3a5f1db08679786799786d4d1543
wdjcodes <wdjcodes@users.noreply.github.com>
2018-10-18 07:50:43 -0400

    Adding files that were missing from last commit

---
+++ client/src/components/CardLink.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <div
+        v-if="!hover"
+        @mouseover="hover = true"
+        class="card border-primary mb-3">
+        <slot></slot>
+      </div>
+      <div
+        v-if="hover"
+        class="card text-white bg-primary mb-3"
+        @mouseover="hover = true"
+        @mouseout="hover = false"
+        @click="pushRoute(route)">
+        <slot></slot>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hover: false,
+    };
+  },
+  props: {
+    route: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    pushRoute(route){
+      this.$router.push(route);
+    }
+  }
+}
+</script>
+
+<style>
+</style>
